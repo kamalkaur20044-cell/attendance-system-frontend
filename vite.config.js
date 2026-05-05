dbf94ca76b3e303ea60133ef76aea500c75f5f33
kamal Kaur <kamalkaur20044@gmail.com>
2026-05-05 17:43:23 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+base:"/attendance-system-frontend/",
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-       target: 'https://attendanceSystem-backend-sk1n.onrender.com',
-        changeOrigin: true
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //      target: 'https://attendanceSystem-backend-sk1n.onrender.com',
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 })
